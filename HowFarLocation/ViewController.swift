d1eceb994b24d2f5c3e470555356e4b23d500d8a
Petr Reshetin <preshetin@gmail.com>
2016-11-17 19:31:31 +0300

    Basic MapKit implementation

--- HowFarLocation/ViewController.swift
+++ HowFarLocation/ViewController.swift
@@ -7,12 +7,66 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var circle = MKCircle()
+    var initialRegionNeedsSetting = true
+    var circleNeedsSetting = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        mapView.showsUserLocation = true
+        self.mapView.delegate = self
+        
+        
+        let coordinate1 = CLLocation(latitude: 53.874798, longitude: 27.675941)
+        let coordinate2 = CLLocation(latitude:  32.089471, longitude: 34.784712)
+        let distanceInMeters = coordinate1.distance(from: coordinate2) / 1000
+        print("Minsk telaviv: \(distanceInMeters) km")
+        
+        
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        if initialRegionNeedsSetting  {
+            initialRegionNeedsSetting = false
+            let latDetla: CLLocationDegrees = 0.1
+            let lonDelta: CLLocationDegrees = 0.1
+            let span = MKCoordinateSpan(latitudeDelta: latDetla, longitudeDelta: lonDelta)
+            let region = MKCoordinateRegion(center: mapView.userLocation.coordinate, span: span)
+            mapView.setRegion(region, animated: false)
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            let circle = MKCircleRenderer(overlay: overlay)
+            circle.strokeColor = UIColor.red
+            circle.lineWidth = 1
+            return circle
+    }
+
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            if circleNeedsSetting {
+                circleNeedsSetting = false
+                mapView.delegate = self
+                circle = MKCircle(center: location.coordinate, radius: 2485685 as CLLocationDistance)
+                mapView.add(circle)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
