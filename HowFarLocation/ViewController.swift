3d466162a6708657c3fbea6be372a2cd03a505c5
Petr Reshetin <preshetin@gmail.com>
2016-11-17 10:08:35 +0300

    Initial Commit

---
+++ HowFarLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HowFarLocation
+//
+//  Created by Petr Reshetin on 17/11/2016.
+//  Copyright © 2016 Petr Reshetin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
