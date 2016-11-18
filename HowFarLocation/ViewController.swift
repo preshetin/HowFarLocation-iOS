96079a0d14ea2238b20f24221b409d8110664f0c
Petr Reshetin <preshetin@gmail.com>
2016-11-18 16:24:16 +0300

    Small fixes

--- HowFarLocation/ViewController.swift
+++ HowFarLocation/ViewController.swift
@@ -10,34 +10,23 @@ import UIKit
 import GoogleMaps
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-
+    
     var radius = 1
     var userLocation: CLLocation? = nil
     
     
     @IBOutlet weak var mapView: GMSMapView!
     @IBOutlet weak var radiusLabel: UILabel!
-
+    
     @IBAction func stepperChanged(_ sender: UIStepper) {
         let intValue = Int(sender.value)
         radiusLabel.text = "\(intValue) km"
         radius = intValue
-        
-        guard let loc = userLocation else {
-            print("userLocation not set.")
-            return
-        }
-        
-        if circleNeedsSetting {
-            mapView.clear()
-            let circ = GMSCircle(position: loc.coordinate, radius: Double(Double(radius) * 1000))
-            circ.map = mapView
-        }
+        putCircle(withRadius: radius)
     }
     
     let locationManager = CLLocationManager()
     var initialRegionNeedsSetting = true
-    var circleNeedsSetting = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,27 +37,52 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
         
         mapView.isMyLocationEnabled = true
-        mapView.settings.myLocationButton = true
-        
     }
     
+    private func putCircle(withRadius raduis: Int) {
+        guard let loc = userLocation else {
+            print("userLocation not set.")
+            return
+        }
+        
+        mapView.clear()
+        let circ = GMSCircle(position: loc.coordinate, radius: Double(Double(radius) * 1000))
+        circ.strokeWidth = 10
+        circ.strokeColor = UIColor(colorLiteralRed: 14.0/255, green: 122.0/255, blue: 254.0/255, alpha: 0.3)
+        // colorWithRed: green: blue: alpha:1.0
+        let update = GMSCameraUpdate.fit(circ.bounds())
+        mapView.animate(with: update)
+        circ.map = mapView
+
+    }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
+            userLocation = location
             if initialRegionNeedsSetting {
                 initialRegionNeedsSetting = false
-                let camera = GMSCameraPosition.camera(withTarget: location.coordinate, zoom: 11)
-                mapView.camera = camera
+                putCircle(withRadius: radius)
             }
-            userLocation = location
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension GMSCircle {
+    func bounds () -> GMSCoordinateBounds {
+        func locationMinMax(positive : Bool) -> CLLocationCoordinate2D {
+            let sign:Double = positive ? 1 : -1
+            let dx = sign * self.radius  / 6378000 * (180/M_PI)
+            let lat = position.latitude + dx
+            let lon = position.longitude + dx / cos(position.latitude * M_PI/180)
+            return CLLocationCoordinate2D(latitude: lat, longitude: lon)
+        }
+        
+        return GMSCoordinateBounds(coordinate: locationMinMax(positive: true),
+                                   coordinate: locationMinMax(positive: false))
+    }
+}
